5af274ebde21bce3dd2a82ff7e8a8203ba650675
Ashish Singh <ashish1qwerty1@gmail.com>
2022-11-18 23:34:05 +0530

    Add progress bar

--- functions.py
+++ functions.py
@@ -2,20 +2,42 @@ from pytube import YouTube
 
 import variables
 
+# this is for manual progress bar
+from tqdm import tqdm
+import time
+
+# for in-built progress bar of pytube
+from pytube.cli import on_progress 
+# its not good looking but fine
+
 def display_title():
+    """
+    Display the title
+    """
     print("Video found: {}".format(variables.TITLE))
+
+def get_size(link):
+    """
+    Display the download size
+    """
+    youtubeObject = YouTube(link)
+    size = youtubeObject.streams.get_highest_resolution().filesize
+    print("Download size: {}".format(size))
 
 def download_video(link):
     """
-    function to download video from the link.
+    function to download video from the link
     """
 
-    youtubeObject = YouTube(link)
+    # to get the progress bar while downloading pass the on_progress_callback parameter with on_progress
+    youtubeObject = YouTube(link, on_progress_callback = on_progress)
     # other arguments are;
     # youtubeObject = YouTube(link, on_progress_callback, oc_complete_callback, proxies, use_oauth, allow_oauth_cache)
     
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     # .stream.get_highest_resolution() will automatically download the highest resolution available
+
+    
 
     try:
         youtubeObject.download()
@@ -24,3 +46,10 @@ def download_video(link):
         print("An error has occurred!!!")
     
     print("Download successful")
+
+def progress_bar():
+    j = 1
+    for i in tqdm(range(100)):
+        # time.sleep(0.01 * j)
+        time.sleep(0.01)
+        j += 1    
--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from functions import download_video, display_title
 from variables import LINK
 
+from functions import progress_bar
+
+from functions import get_size
+
 def run():
+    # progress_bar()
+    get_size(link = LINK)
     display_title()
     download_video(link = LINK)
 
